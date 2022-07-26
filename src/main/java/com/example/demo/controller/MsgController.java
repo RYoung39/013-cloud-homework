e4bee777f3df7bdec30adf49b3904379c42208f4
RuiYang <1594643089@qq.com>
2022-07-26 11:16:05 +0800

    prometheus

--- src/main/java/com/example/demo/controller/MsgController.java
+++ src/main/java/com/example/demo/controller/MsgController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.MsgService;
+import io.micrometer.core.annotation.Timed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ public class MsgController {
     @Autowired
     private MsgService msgService;
 
+    @Timed(value = "msg_request_duration", description = "Time taken to return msg", histogram = true)
     @GetMapping("/msg")
     public String getMsg(@RequestParam String msg){
         return msgService.getMsg(msg);
